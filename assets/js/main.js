8d489b454729a90f96dbb95d893c4ad250964420
Md Ariful Islam <imdarif122@gmail.com>
2022-11-13 22:33:15 +0600

    Fix navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -371,7 +371,7 @@
 
         mobileMenuActive: function (e) {
 
-            $('.humberger-menu').on('click', function (e) {
+            $('#menuBtn').on('click', function (e) {
                 e.preventDefault();
                 $('.popup-mobile-menu').addClass('menu-open');
                 imJs._html.css({
